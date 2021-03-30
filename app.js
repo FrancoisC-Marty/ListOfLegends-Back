096b1b8b4dde18bbf05a9c84e927940c9ebac1aa
Kyoshiro Mibu <francois.caus@gmail.com>
2021-03-30 19:49:08 +0200

    cors issue test 4

--- app.js
+++ app.js
@@ -9,31 +9,29 @@ const app = express();
 
 DB.connect();
 
-// const allowlist = ['https://listoflegends.surge.sh/', 'http://localhost:3000'];
-// /**
-//  * This function allows or not cors depending on the allowlist established
-//  * @param {*} req 
-//  * @param {*} callback 
-//  */
-// const corsOptionsDelegate = (req, callback) => {
-//   let corsOptions;
+const allowlist = ['http://localhost:3000', 'https://listoflegends.surge.sh/'];
+/**
+ * This function allows or not cors depending on the allowlist established
+ * @param {*} req 
+ * @param {*} callback 
+ */
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
 
-//   let isDomainAllowed = allowlist.indexOf(req.header('Origin')) !== -1;
+  let isDomainAllowed = allowlist.indexOf(req.header('Origin')) !== -1;
 
-//   if (isDomainAllowed) {
-//       corsOptions = {
-//         origin: true,
-//         method: 'GET, POST',
-//       }
-//   } else {
-//       corsOptions = { origin: false }
-//   }
-//   callback(null, corsOptions)
-// }
+  if (isDomainAllowed) {
+      corsOptions = {
+        origin: true,
+        method: 'GET, POST',
+      }
+  } else {
+      corsOptions = { origin: false }
+  }
+  callback(null, corsOptions)
+}
 
-app.use(cors({
-  origin: 'https://listoflegends.surge.sh/',
-}));
+app.use(cors(corsOptionsDelegate));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
